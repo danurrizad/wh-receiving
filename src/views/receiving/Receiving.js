57d658ee54cff8b62ab8a00510991900ab94830b
danurrizad <danurrizad242@gmail.com>
2025-01-13 17:51:20 +0700

    feat: initial view design

---
+++ src/views/receiving/Receiving.js
@@ -0,0 +1,160 @@
+import React, { useState, useEffect} from 'react'
+import CIcon from '@coreui/icons-react'
+import * as icon from '@coreui/icons'
+import { CButton, CButtonGroup, CCard, CCardBody, CCardHeader, CCardTitle, CCol, CContainer, CFormInput, CFormText, CInputGroup, CRow, CTable, CTableBody, CTableDataCell, CTableHead, CTableHeaderCell, CTableRow } from '@coreui/react'
+import { dataReceivingDummy } from '../../utils/DummyData'
+import  colorStyles from '../../utils/StyleReactSelect'
+import Select from 'react-select'
+import Pagination from '../../components/Pagination'
+
+const Receiving = () => {
+  const [ dataReceiving, setDataReceiving ] = useState(dataReceivingDummy)
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(10)
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems =
+  dataReceiving.length > 0
+        ? dataReceiving.slice(indexOfFirstItem, indexOfLastItem)
+        : dataReceiving.slice(indexOfFirstItem, indexOfLastItem)
+
+  useEffect(()=>{
+     const currentItems = dataReceiving.length > 0
+        ? dataReceiving.slice(indexOfFirstItem, indexOfLastItem)
+        : dataReceiving.slice(indexOfFirstItem, indexOfLastItem)
+  }, [currentPage])
+
+  const options = [
+    // { value: 'all', label: 'All' },
+    { value: 'Shortage', label: 'Shortage' },
+    { value: 'Optimal', label: 'Optimal' },
+  ]
+
+  const [ showCard, setShowCard ] = useState({
+    schedule: true,
+    receiving: true
+  })
+
+  const getDataReceiving = () => {
+    setDataReceiving(dataReceivingDummy)
+  }
+
+ 
+
+  const handleFilterReceiving = (selectedOption) => {
+    if (!selectedOption) {
+      // If no option selected, reset to all data
+      setDataReceiving(dataReceivingDummy);
+    } else {
+      // Filter based on the selected status
+      const filtered = dataReceivingDummy.filter(
+        (item) => item.status === selectedOption.value
+      );
+      setDataReceiving(filtered);
+    }
+  }
+
+ 
+
+
+  
+  useEffect(()=>{
+    getDataReceiving()
+  }, [])
+
+  return (
+    <CContainer fluid>
+        <CRow>
+            <CCard className='p-0'>
+                <CCardHeader>
+                    <CCardTitle>INPUT</CCardTitle>
+                </CCardHeader>
+                <CCardBody>
+                  <CRow className='mb-3'>
+                    <CCol xs={4} className='d-flex align-items-center'>
+                        <CFormText className='w-25'>DN No</CFormText>
+                        <CInputGroup className='d-flex align-items-center'>
+                          <CFormInput/>
+                          <CButton color='info' style={{ color: 'white'}}>Find</CButton>
+                        </CInputGroup>
+                    </CCol>
+                  </CRow>
+                  <CRow>
+                    <CCol sm={4} className='d-flex align-items-center'>
+                        <CFormText className='w-25'>Material</CFormText>
+                        <CInputGroup className='d-flex align-items-center'>
+                          <CFormInput disabled/>
+                        </CInputGroup>
+                    </CCol>
+                    <CCol sm={4} className='d-flex align-items-center'>
+                      <CFormText className='w-25'>Quantity</CFormText>
+                        <CInputGroup className='d-flex align-items-center'>
+                          <CFormInput disabled/>
+                        </CInputGroup>
+                    </CCol>
+                    <CCol sm={4} className='d-flex justify-content-end'>
+                      <CButton color='success' style={{color: 'white'}} className='flex-grow-0 d-flex align-items-center gap-2'>
+                        <CIcon icon={icon.cilCheckAlt}/>
+                        <div style={{border: "0.5px solid white", height: "10px", width: "2px"}}></div>
+                        <span>Submit</span>
+                      </CButton>
+                    </CCol>
+                  </CRow>
+                </CCardBody>
+            </CCard>
+        </CRow>
+        <CRow className='pt-4'>
+          <CCard className='p-0'>
+            <CCardHeader>
+              <CCardTitle>SCHEDULE DATA</CCardTitle>
+            </CCardHeader>
+            <CCardBody>
+              <CRow>
+                <CCol>
+                  <CButton color='info' style={{color: 'white'}} className='flex-grow-0 d-flex align-items-center gap-2'>
+                    <CIcon icon={icon.cilCloudUpload}/>
+                    <div style={{border: "0.5px solid white", height: "10px", width: "1px"}}></div>
+                    <span>Upload a File</span>
+                  </CButton>
+                </CCol>
+              </CRow>
+              <CRow className='p-2 pt-3'>
+                              <CTable bordered>
+                                <CTableHead>
+                                  <CTableRow>
+                                    <CTableHeaderCell>DN No</CTableHeaderCell>
+                                    <CTableHeaderCell>Material No</CTableHeaderCell>
+                                    <CTableHeaderCell>Material Description</CTableHeaderCell>
+                                    <CTableHeaderCell>Req Quantity</CTableHeaderCell>
+                                    <CTableHeaderCell>Actual Quantity</CTableHeaderCell>
+                                    <CTableHeaderCell>Difference</CTableHeaderCell>
+                                    <CTableHeaderCell>Date</CTableHeaderCell>
+                                  </CTableRow>
+                                </CTableHead>
+                                <CTableBody>
+                                  { dataReceiving.map((data, index)=>{
+                                    return(
+                                      <CTableRow key={index} color={`${data.difference !== 0 ? "danger" : ""}`}>
+                                      {/* <CTableRow key={index} style={{ backgroundColor: `${data.difference !== 0 ? "red" : ""}`}}> */}
+                                        <CTableDataCell>{data.dn_no}</CTableDataCell>
+                                        <CTableDataCell>{data.material_no}</CTableDataCell>
+                                        <CTableDataCell>{data.material_desc}</CTableDataCell>
+                                        <CTableDataCell>{data.req_qty}</CTableDataCell>
+                                        <CTableDataCell>{data.actual_qty}</CTableDataCell>
+                                        <CTableDataCell>{data.difference}</CTableDataCell>
+                                        <CTableDataCell>{data.date}</CTableDataCell>
+                                      </CTableRow>
+                                    )
+                                  })}
+                                </CTableBody>
+                              </CTable>
+                            </CRow>
+            </CCardBody>
+          </CCard>
+        </CRow>
+    </CContainer>
+  )
+}
+
+export default Receiving
---
+++ src/views/schedule/Schedule.js
@@ -0,0 +1,200 @@
+import React, { useState, useEffect} from 'react'
+import CIcon from '@coreui/icons-react'
+import * as icon from '@coreui/icons'
+import { CButton, CButtonGroup, CCard, CCardBody, CCardHeader, CCardTitle, CCol, CContainer, CFormInput, CFormLabel, CFormText, CInputGroup, CRow, CTable, CTableBody, CTableDataCell, CTableHead, CTableHeaderCell, CTableRow } from '@coreui/react'
+import { dataSchedulesDummy, dataReceivingDummy } from '../../utils/DummyData'
+import  colorStyles from '../../utils/StyleReactSelect'
+import Select from 'react-select'
+import Pagination from '../../components/Pagination'
+
+const Schedule = () => {
+  const [ dataSchedules, setDataSchedules ] = useState(dataSchedulesDummy)
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(10)
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems =
+    dataSchedules.length > 0
+        ? dataSchedules.slice(indexOfFirstItem, indexOfLastItem)
+        : dataSchedules.slice(indexOfFirstItem, indexOfLastItem)
+
+  useEffect(()=>{
+     const currentItems = dataSchedules.length > 0
+        ? dataSchedules.slice(indexOfFirstItem, indexOfLastItem)
+        : dataSchedules.slice(indexOfFirstItem, indexOfLastItem)
+  }, [currentPage])
+
+  const options = [
+    // { value: 'all', label: 'All' },
+    { value: 'Shortage', label: 'Shortage' },
+    { value: 'Optimal', label: 'Optimal' },
+  ]
+
+  const [ showCard, setShowCard ] = useState({
+    schedule: true,
+    receiving: true
+  })
+
+  const getDataSchedules = () => {
+    setDataSchedules(dataSchedulesDummy)
+  }
+
+ 
+
+  const handleFilterSchedule = (selectedOption) => {
+    if (!selectedOption) {
+      // If no option selected, reset to all data
+      setDataSchedules(dataSchedulesDummy);
+    } else {
+      // Filter based on the selected status
+      const filtered = dataSchedulesDummy.filter(
+        (item) => item.status === selectedOption.value
+      );
+      setDataSchedules(filtered);
+    }
+  }
+
+ 
+
+
+  
+  useEffect(()=>{
+    getDataSchedules()
+  }, [])
+
+  return (
+    <CContainer fluid>
+        <CRow>
+            <CCard className='p-0'>
+                <CCardHeader>
+                    <CCardTitle>INPUT</CCardTitle>
+                </CCardHeader>
+                <CCardBody className='d-flex'>
+                  <CCol sm={7} className='d-flex'>
+                    <CRow>
+                      <CCol sm={4}>
+                        <CRow className='mb-3'>
+                          <CFormLabel>DATE</CFormLabel>
+                          <div className='d-flex align-items-center gap-2'>
+                            <CFormInput/>
+                            <CIcon style={{}} icon={icon.cilCalendar} size='xxl' className='flex-shrink-0'/>
+                          </div>
+                        </CRow>
+                        <CRow>
+                          <CFormLabel>VENDOR CODE</CFormLabel>
+                          <div className='d-flex align-items-center gap-2'>
+                            <CFormInput/>
+                            <CIcon style={{}} icon={icon.cilBarcode} size='xxl' className='flex-shrink-0'/>
+                          </div>
+                        </CRow>
+                      </CCol>
+                      <CCol sm={4} className='d-flex flex-column'>
+                        VENDOR STATUS
+                        {/* <CCard className='p-1 mt-2 h-100' style={{ backgroundColor:"rgb(152, 193, 255)", borderRadius: "10px"}}> */}
+                        <CCard className='p-1 mt-2 h-100' style={{ backgroundColor:"gray", borderRadius: "10px"}}>
+                          <CTable className=''>
+                            <CTableRow>
+                              <CTableDataCell>Vendor Code</CTableDataCell>
+                              <CTableDataCell>:</CTableDataCell>
+                              <CTableDataCell>123192</CTableDataCell>
+                            </CTableRow>
+                            <CTableRow>
+                              <CTableDataCell>ARRIVAL [PLAN]</CTableDataCell>
+                              <CTableDataCell>:</CTableDataCell>
+                              <CTableDataCell>10-01-2025</CTableDataCell>
+                            </CTableRow>
+                            <CTableRow>
+                              <CTableDataCell>ARRIVAL [ACT]</CTableDataCell>
+                              <CTableDataCell>:</CTableDataCell>
+                              <CTableDataCell>10-01-2025</CTableDataCell>
+                            </CTableRow>
+                          </CTable>
+                        </CCard>
+                      </CCol>
+                      <CCol sm={4} className='d-flex flex-column'>
+                        <CRow>SCHEDULE STATUS</CRow>
+                        <CRow className='h-100'>
+                          <CCard className='p-5 d-flex align-items-center justify-content-center'>
+                            <h1>ON TIME</h1>
+                          </CCard>
+                        </CRow>
+                      </CCol>
+                    </CRow>
+                  </CCol>
+                  <CCol sm={5} className='d-flex justify-content-end align-items-end'>
+                      <CButton color='success' style={{color: 'white'}} className='flex-grow-0 d-flex align-items-center gap-2'>
+                        <CIcon icon={icon.cilCheckAlt}/>
+                        <div style={{border: "0.5px solid white", height: "10px", width: "1px"}}></div>
+                        <span>Submit</span>
+                      </CButton>
+                  </CCol>
+                </CCardBody>
+            </CCard>
+        </CRow>
+        <CRow className='pt-4'>
+          <CCard className='p-0'>
+            <CCardHeader>
+              <CCardTitle>SCHEDULE DATA</CCardTitle>
+            </CCardHeader>
+            <CCardBody>
+              <CRow>
+                <CCol>
+                  <CButton color='info' style={{color: 'white'}} className='flex-grow-0 d-flex align-items-center gap-2'>
+                    <CIcon icon={icon.cilCloudUpload}/>
+                    <div style={{border: "0.5px solid white", height: "10px", width: "1px"}}></div>
+                    <span>Upload a File</span>
+                  </CButton>
+                </CCol>
+              </CRow>
+              <CRow className='p-3'>
+                  <CTable bordered>
+                    <CTableHead>
+                      <CTableRow align='middle'>
+                        <CTableHeaderCell rowSpan={2}>No</CTableHeaderCell>
+                        <CTableHeaderCell rowSpan={2}>Vendor ID</CTableHeaderCell>
+                        <CTableHeaderCell rowSpan={2}>Vendor Name</CTableHeaderCell>
+                        <CTableHeaderCell rowSpan={2}>Day</CTableHeaderCell>
+                        <CTableHeaderCell rowSpan={2}>Date</CTableHeaderCell>
+                        <CTableHeaderCell colSpan={2}>Schedule Time</CTableHeaderCell>
+                        <CTableHeaderCell rowSpan={2}>Arrival Time</CTableHeaderCell>
+                        <CTableHeaderCell rowSpan={2}>Status</CTableHeaderCell>
+                        <CTableHeaderCell rowSpan={2}>Delay Time</CTableHeaderCell>
+                      </CTableRow>
+                      <CTableRow>
+                        <CTableHeaderCell >From</CTableHeaderCell>
+                        <CTableHeaderCell >To</CTableHeaderCell>
+                      </CTableRow>
+                    </CTableHead>
+                    <CTableBody>
+                        { currentItems.map((data, index)=> {
+                          return(
+                            <CTableRow key={index}>
+                              <CTableDataCell>{index+1}</CTableDataCell>
+                              <CTableDataCell>{data.vendor_id}</CTableDataCell>
+                              <CTableDataCell>{data.vendor_name}</CTableDataCell>
+                              <CTableDataCell>{data.day}</CTableDataCell>
+                              <CTableDataCell>{data.date}</CTableDataCell>
+                              <CTableDataCell>{data.schedule_from}</CTableDataCell>
+                              <CTableDataCell>{data.schedule_to}</CTableDataCell>
+                              <CTableDataCell>{data.arrival_time}</CTableDataCell>
+                              <CTableDataCell className='text-center'>
+                                <div className="py-1 px-2 " style={{ backgroundColor: data.status === "Delayed" ? "#F64242" : "#35A535", color: 'white', borderRadius: '5px'}}>
+                                  {data.status}  
+                                  </div>
+                                </CTableDataCell>
+                              <CTableDataCell style={{ color: data.status === "Delayed" ? "#F64242" : ""}}> {data.delay_time !== 0 ? `- ${data.delay_time}` : ""}</CTableDataCell>
+                            </CTableRow>
+                          )
+                        })}
+                    </CTableBody>
+                  </CTable>
+              </CRow>
+            </CCardBody>
+          </CCard>
+        </CRow>
+    </CContainer>
+  )
+}
+
+export default Schedule
