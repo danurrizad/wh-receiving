a6a0891b7257f54f3e21c25a255a7f5a31e13c4e
danurrizad <danurrizad242@gmail.com>
2025-01-13 17:52:02 +0700

    add: routes for Schedule and Receiving

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+const Schedule = React.lazy(() => import('./views/schedule/Schedule'))
+const Receiving = React.lazy(() => import('./views/receiving/Receiving'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/schedule', name: 'Schedule', element: Schedule },
+  { path: '/receiving', name: 'Receiving', element: Receiving },
 ]
 
 export default routes
